70d3a52b95d32886af61e5292a777110da0db0fd
Kaio Sumikawa <kaiosumikawa@gmail.com>
2025-08-13 20:18:06 -0300

    aula 13/08/2025

--- Javascript/01.1/js-01.1.js
+++ Javascript/01.1/js-01.1.js
@@ -34,3 +34,35 @@ function userMessage(user) {
   // Used to insert variables inside STRINGS (BACKTICKS)
 }
 userMessage(user); // ARGUMENT
+
+// FUNÇÃO COM MAIS DE UM PARÂMETROS
+const n1 = 10;
+const n2 = 12;
+
+function mult(n1,n2) {
+  // Essa função recebe dois parâmetros
+  let result = n1 * n2
+  document.write(`A multiplicação de ${n1} e ${n2} é igual a ${result}`)
+}
+mult(n1,n2)
+
+// FUNÇÃO COM RETORNO
+const num1 = 1000
+const num2 = 5;
+
+function div(num1, num2) {
+  return num1 / num2
+}
+document.write(`<p>A divisão de ${num1} por ${num2} é igual a ${div(num1,num2)}`)
+
+// FUNÇÃO COM DIFERENTES RETORNOS
+const number = 7;
+
+function parImpar(number) {
+  if (number % 2 == 0) {
+    return 'par'
+  } else {
+    return "impar";
+  }
+}
+document.write(`<p>O número ${number} é <strong>${parImpar(number)}</strong>.`)
