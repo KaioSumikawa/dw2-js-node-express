014b8e93e6d8289ef8e4cf86cc0ac0dd151d79de
Kaio Sumikawa <kaiosumikawa@gmail.com>
2025-08-19 01:05:56 +0900

    Exercício 02

--- Lista de Exercícios/13.08.2025/Exercício 01/main.js
+++ Lista de Exercícios/13.08.2025/Exercício 01/main.js
@@ -1,7 +1,7 @@
 const user = "Diego Max";
 
 function saudacaoPersonalizada(nome) {
-  document.write("Olá, " + nome + "Bem-vindo(a) à Calculadora Universal!");
+  document.write("Olá, " + nome + "! Bem-vindo(a) à Calculadora Universal!");
 }
 
 saudacaoPersonalizada(user);
---
+++ Lista de Exercícios/13.08.2025/Exercício 02/main.js
@@ -0,0 +1,26 @@
+function operacaoMatematica(num1, operador, num2) {
+    let resultado;
+
+    if (operador === '+') {
+        resultado = num1 + num2;
+    } else if (operador === '-') {
+        resultado = num1 - num2;
+    } else if (operador === '*') {
+        resultado = num1 * num2;
+    } else if (operador === '/') {
+        if (num2 !== 0) {
+            resultado = num1 / num2;
+        } else {
+            document.write("Erro: Divisão por zero não é permitida!");
+            return;
+        }
+    } else {
+        document.write("Erro: Operador inválido!");
+        return;
+    }
+
+    document.write("O resultado de " + num1 + " " + operador + " " + num2 + " é igual a " + resultado + ".");
+}
+
+// Teste
+operacaoMatematica(10, '+', 5);
