aa1616b3d740832a18082b55fecaa98930a735f6
Kaio Sumikawa <kaiosumikawa@gmail.com>
2025-08-19 02:21:21 +0900

    Exercício 03

---
+++ Lista de Exercícios/13.08.2025/Exercício 03/main.js
@@ -0,0 +1,17 @@
+const calcularDobro = function(numero) {
+    return numero * 2;
+}
+
+document.write('<h3>Resultados dos cálculos:</h3>');
+
+let numero1 = 5;
+let resultado1 = calcularDobro(numero1);
+document.write(`<p>O dobro de ${numero1} é: ${resultado1}</p>`);
+
+let numero1 = 10;
+let resultado1 = calcularDobro(numero2);
+document.write(`<p>O dobro de ${numero2} é: ${resultado2}</p>`);
+
+let numero1 = 15;
+let resultado1 = calcularDobro(numero3);
+document.write(`<p>O dobro de ${numero3} é: ${resultado3}</p>`);
