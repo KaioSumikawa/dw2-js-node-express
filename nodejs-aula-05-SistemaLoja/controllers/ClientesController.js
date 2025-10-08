330cc1f8bdbe2096edfeebd79ee1a61b11715244
Kaio Sumikawa <kaiosumikawa@gmail.com>
2025-10-08 19:41:07 -0300

    aula

--- nodejs-aula-05-SistemaLoja/controllers/ClientesController.js
+++ nodejs-aula-05-SistemaLoja/controllers/ClientesController.js
@@ -7,10 +7,33 @@ import Cliente from "../models/Cliente.js";
 router.get("/clientes", function (req, res) {
   // SELECT * FROM CLIENTES
   Cliente.findAll().then(clientes => {
+    // then => Promessa bem sucedida
     res.render("clientes", {
       clientes: clientes,
     });
+    // catch => Falha na resolução da promessa
+  }).catch(error => {
+    console.log(error)
   });
 });
+
+// ROTA DE CADASTRO DE CLIENTES
+router.post("/clientes/new", (req, res) => {
+  // COLETANDO OS DADOS DO FORMULÁRIO
+  const nome = req.body.nome
+  const cpf = req.body.cpf
+  const endereco = req.body.endereco
+  Cliente.create({
+    // coluna : dado do form
+    nome : nome,
+    cpf : cpf,
+    endereco : endereco
+  }).then(() => {
+    res.redirect("/clientes")
+  }).catch(error => {
+    console.log(error);
+  });
+});
+
 
 export default router;
