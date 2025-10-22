32186ec354143879ff9cd1304107a1b57511834f
Kaio Sumikawa <kaiosumikawa@gmail.com>
2025-10-22 18:52:51 -0300

    aula

---
+++ SistemaLoja - Relacional/controllers/PedidosController.js
@@ -0,0 +1,43 @@
+import express from "express";
+import Pedido from "../models/Pedido.js";
+const router = express.Router();
+
+// ROTA PEDIDOS
+router.get("/pedidos", function (req, res) {
+  Pedido.findAll()
+    .then((pedidos) => {
+      res.render("pedidos", {
+        pedidos: pedidos,
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
+});
+
+// ROTA DE CADASTRO DE PEDIDOS
+router.post("/pedidos/new", (req, res) => {
+  const numero = req.body.numero;
+  const valor = req.body.valor;
+  Pedido.create({
+    numero: numero,
+    valor: valor,
+  }).then(() => {
+      res.redirect("/pedidos");
+    }).catch((err) => {
+      console.log(err);
+    });
+});
+
+// ROTA DE EXCLUSÃO DE CLIENTES
+router.get("/pedidos/delete/:id", (req, res) => {
+  const id = req.params.id;
+  Pedido.destroy({
+    where: {id: id}
+  }).then(() => {
+      res.redirect("/pedidos");
+    }).catch((err) => {
+      console.log(err);
+    });
+});
+
+export default router;
